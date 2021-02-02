4e0ab0c5724973859a21d3f8b7db86adbbb2c58a
pigbbs <78332657+pigbbs@users.noreply.github.com>
2021-02-02 09:47:43 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,6 +1,7 @@
 if (typeof Bmob == "undefined")
   alert("You must include 'bmobSDK.js' to connect the database.");
 var db = {};
+Bmob. initialize("666aff82f39ad4c7","081126");
 db.readdata = function (objid,usecache)
 {
     if (usecache)
@@ -8,6 +9,12 @@ db.readdata = function (objid,usecache)
        var obj = {};
        var content = sessionStorage.getItem("cache-" + objid);
        if (content == null)
-       {}
+       {
+           obj.status = 400;
+       } else {
+           obj.status = 200;
+           obj.content = content;
+       }
+       return obj;
     }
 };
