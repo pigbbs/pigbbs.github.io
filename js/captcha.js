d2b50da158e5118dc19c88e0c41735e5007b04ec
pigbbs <78332657+pigbbs@users.noreply.github.com>
2021-02-02 15:27:34 +0800

    Update captcha.js

--- js/captcha.js
+++ js/captcha.js
@@ -1,5 +1,6 @@
 function newcaptcha(canvasid)
 {
+   try {
    var arr = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
    var str = "";
    var i = 0;
@@ -16,6 +17,7 @@ function newcaptcha(canvasid)
    ctx.fillStyle="#ff0000";
    ctx.font = "40px Arial";
    ctx.fillText(str,0,0);
+   } catch (err) { alert(err); }
 }
 function checkcaptcha(textid)
 {
