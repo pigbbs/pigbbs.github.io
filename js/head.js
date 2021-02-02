a91ac2548358184f4093d3181a82f4b82f4dacd6
pigbbs <78332657+pigbbs@users.noreply.github.com>
2021-02-02 14:31:40 +0800

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,2 +1,20 @@
 if (sessionStorage.getItem("captcha") == null)
-  location.href = "/captcha";
+  location.href = "/captcha#" + location.href;
+function checklogin()
+{
+  if (sessionStorage.getItem("uuid") != null && sessionStorage.getItem("username") != null)
+    location.href = "/login#" + locatiob.href;
+}
+function checkrobot()
+{
+   var updatetime = sessionStorage.getItem("updatetime");
+   updatetime = parseInt(updatetime);
+   var mstime = new Date().getTime();
+   if ((mstime - updatetime) < 30 * 1000)
+     return true;
+   else
+   {
+     sessionStorage.setItem("updatetime",mstime);
+     return false;
+   }
+}
