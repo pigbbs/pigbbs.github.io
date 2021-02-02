931821e5a94f6a30b3e5580eaa456bdea42cc739
pigbbs <78332657+pigbbs@users.noreply.github.com>
2021-02-02 13:54:02 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -10,11 +10,47 @@ db.readdata = function (objid,usecache)
        var content = sessionStorage.getItem("cache-" + objid);
        if (content == null)
        {
-           obj.status = 400;
+           obj.status = 500;
        } else {
            obj.status = 200;
            obj.content = content;
        }
        return obj;
-    }
+    } else {
+        var obj = {};
+        var query = Bmob.Query("chat");
+        query.get(objid).then(res => {
+            obj.status = 200;
+            obj.content = res;
+            sessionStorage.setItem("cache-" + objid,res.content);
+        }).catch(err => {
+            obj.status = 400;
+            obj.content = err;
+        });
+        return obj;
+     }
+};
+db.savedata = function(objid,dat) {
+   var obj = {};
+   var query = Bmob.Query("chat");
+   query.set("content",dat);
+   query.save(res => {
+        obj.status = 200;
+        obj.result = res;
+   }).catch(err => {
+       obj.status = 400;
+       obj.result = err;
+   });   
+};
+db.newbbs = function () {
+
+     var obj = {};
+     var query = Bmob.Query("chat");
+     query.save(res => {
+          obj.status = 200;
+          obj.result = res;
+     }).catch(err => {
+          obj.status = 400;
+          obj.result = err;
+     });
 };
