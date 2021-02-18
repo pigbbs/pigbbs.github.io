a4e36476c7e711f3cc6cfe96c6729bd374e8e1b2
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-02-18 13:14:44 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,70 +2,34 @@ if (typeof Bmob == "undefined")
   alert("You must include 'bmobSDK.js' to connect the database.");
 var db = {};
 Bmob. initialize("666aff82f39ad4c7","081126");
-db.readdata = function (objid,usecache)
-{
-    if (usecache)
-    {
-       var obj = {};
-       var content = sessionStorage.getItem("cache-" + objid);
-       if (content == null)
-       {
-           obj.status = 500;
-       } else {
-           obj.status = 200;
-           obj.content = content;
-       }
-       return obj;
-    } else {
-        var obj = {};
-        var query = Bmob.Query("chat");
-        query.get(objid).then(res => {
-            obj.status = 200;
-            obj.content = res;
-            sessionStorage.setItem("cache-" + objid,res.content);
-        }).catch(err => {
-            obj.status = 400;
-            obj.content = err;
-        });
-        return obj;
-     }
-};
-db.savedata = function(objid,dat) {
-   var obj = {};
-   var query = Bmob.Query("chat");
-   query.set("content",dat);
-   query.save(res => {
-        obj.status = 200;
-        obj.result = res;
-        return obj;
-   }).catch(err => {
-       obj.status = 400;
-       obj.result = err;
-       return obj;
-   });   
-};
+
 db.newbbs = function () {
 
      var obj = {};
     var uuid = sessionStorage.getItem("uuid");
     var q = Bmob.Query("userinfo");
     q.get(uuid).then(res => {
+      //得到用户信息
       console.log(res);
       q.set("id",res.objectId);
       q.set("BBSnum",parseInt(res.BBSnum)+1);
+      //检查用户的留言板数量
       if (res.BBSnum > 2)
         alert("错误！您的账户创建的留言板已达上限");
       else
       {
+        //提交用户信息给服务器
         q.save().then(res => {
           console.log(res);
           
            var query = Bmob.Query("chat");
             query.set("content","<strong>欢迎来到猪猪留言板</strong><br>");
+          //初始化留言板
            query.set("level",0);
               query.save().then(res => {
                     obj.status = 200;
                     obj.result = res;
+                //提交留言板给服务器
                      sessionStorage.setItem("boardID",JSON.stringify(obj));
           }).catch(err => {
                    obj.status = 400;
